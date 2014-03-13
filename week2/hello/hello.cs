f4438f7098e2f053182a949fd8cd402716cf9cdd
youngnam <utopia@naver.com>
2014-03-13 17:01:36 +0900

    prostar init

---
+++ week2/hello/hello.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Hoyoung {
+	class HelloNext {
+		static void Main(String[] args) {
+			Console.WriteLine("Hello, Next");
+			Console.WriteLine("Hello, {0}!", args[0]);
+		}
+	}
+}
