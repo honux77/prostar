2c1a186f4cc8f192d2fdab67d32f5463cd61f0b1
youngnam <utopia@naver.com>
2014-03-29 15:11:13 +0900

    string[] example

---
+++ week4/lab4-4.cs
@@ -0,0 +1,16 @@
+using System;
+
+class test {
+	static void Main(string[] args) {
+		string line = Console.ReadLine();
+		string[] words = line.Split(' ');
+		int[] array = new int[words.Length];
+
+		for (int i = 0; i < words.Length; i++)
+			array[i] = Convert.ToInt32(words[i]);
+
+		//bonus
+		foreach (int a in array)
+			Console.WriteLine(a);
+	}
+}
