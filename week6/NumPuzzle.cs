8575486870c33547b3f74d3ee204c5978b24730e
horong54 <honux@honux.net>
2014-04-11 22:22:13 +0900

    week6 class

--- week6/NumPuzzle.cs
+++ week6/NumPuzzle.cs
@@ -15,52 +15,75 @@ public class NumPuzzle {
 				mData[i,j] = i * size + j + 1;
 		mData[size-1,size-1] = 0;
 		xZero = yZero = size -1;
-
 	}
 
+	//return all the number sequences
 	public string FindMove() {
-		//code here
-		"2 3 6 7"
 		string ret ="";
+		//implement
 		return ret;
 	}
 
+	/**
+	 * If possible to move, move the number
+	 * return true if success, else return false
+	 */
+	public bool Move(int num) {
+		return false;
+	}
+
 	private void ZeroLocation() {
-		for (int i = 0; i < mSize; i++)
-			for (int j = 0; j < mSize; j++)
-				if mData[i,j] == 0 {
+		for (int i = 0; i < mSize; i++) {
+			for (int j = 0; j < mSize; j++) {
+				if (mData[i,j] == 0) {
 					xZero = i;
 					yZero = j;
 					return;
 				}
+			}
+		}
 	}
 
 	private bool IsMove(int row, int col) {
-	  //mData[row,col] -- true
-	  // false
 	  ZeroLocation();
+	  if (!IsValid(row, col))
+			  return false;
+	  if (
+	//code here
+	
+	  return true;
+	}
 
+	//check whether row and col values are in valid range
+	//0 <= row, col < mSize
+	private bool IsValid(int row, int col) {
+		return ( 0 <= row && 0<= col && row < mSize && col < mSize);
+	}
+
+	private void swap(ref int a, ref int b) {
+		//code here
 	}
 }
 
 class Test {
-	static void Fail(string msg) {
-		Console.WriteLine(msg);
-		Environment.Exit(1);
-	}
 	static void Main() {
 		NumPuzzle np = new NumPuzzle();
-		
-		np.Init(3); // 1 2 3 4 5 6 7 8 0
-		//init test
-		if (np.mSize != 3) Fail("init test fail");
-		if (np.mData[np.mSize-1,np.mSize-1] != 0) Fail("init test2 fail");
-	
-		Console.WriteLine("init test success");
 		
 		//test 1 
 		string av = np.FindMove();
 		if(av != "6 8") Fail("test 1 fail!!");
 		Console.WriteLine("test1 success!");
+		
+		//test 2
+		
+	}
+
+	static int read(string msg) {
+		return 0;
+	}
+
+	static void Fail(string msg) {
+		Console.WriteLine(msg);
+		Environment.Exit(1);
 	}
 }
